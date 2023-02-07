ef8ba159df208993424f09514b3cf8e18525c36b
firvain <info@xtc.codes>
2023-02-07 10:37:47 +0200

    change Scheduler.py change aps log LEVEL

--- Modules/Scheduler.py
+++ Modules/Scheduler.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename=logname,
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S')
-logging.getLogger('apscheduler').setLevel(logging.ERROR)
+logging.getLogger('apscheduler').setLevel(logging.INFO)
 
 init(autoreset=True)
 
@@ -20,7 +20,7 @@ def my_schedule(job1):
     scheduler = BackgroundScheduler({'apscheduler.timezone': 'Europe/Athens'})
 
     trigger_job1 = CronTrigger(
-        year="*", month="*", day="*", hour="*/12", minute="5", second="0", timezone="Europe/Athens"
+        year="*", month="*", day="*", hour="*/12", minute="15", second="0", timezone="Europe/Athens"
     )
 
     scheduler.add_job(job1, trigger=trigger_job1, name="corrections")
